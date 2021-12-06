dfe0bbcf02f5b16591faea5d7a61a2243d8c6b58
noecan <91905967+noecan@users.noreply.github.com>
2021-12-06 15:54:05 -0600

    Add files via upload

---
+++ DatabaseSeeder.php
@@ -0,0 +1,16 @@
+<? php
+
+use  Illuminate \ Database \ Seeder ;
+
+clase  DatabaseSeeder  extiende  Sembradora
+{
+    / **
+     * Ejecute las semillas de la base de datos.
+     *
+     * @return void
+     * /
+     función  pública ejecutar ()
+    {
+        // $ this-> call (UsersTableSeeder :: class);
+    }
+}
